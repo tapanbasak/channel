490f3233cf61d60e49888389a96eb23f0cb08f1c
Tapan Basak <tapanonly@gmail.com>
2012-07-15 13:11:43 +0530

    update 2nd time

--- testblob.php
+++ testblob.php
@@ -3,7 +3,7 @@
    mysql_select_db("drupal_city", $cp) or die('there is a problem in database connection.');
    $q = mysql_query("SELECT `data` FROM `field_config_instance` WHERE `id`=17");
    $d = mysql_fetch_assoc($q);
-   print '111<pre>';
+   print '2223<pre>';
    print_r(unserialize($d['data']));
    echo date('Y-m-d H;i:s');
 ?>
